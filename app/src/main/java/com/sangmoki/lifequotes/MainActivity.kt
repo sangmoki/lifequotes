ec4a79a218245d379a8ab2b7a4566072622e43e5
이상목 <yoochang4176@naver.com>
2024-05-23 14:37:14 +0900

    Feat: "first commit"

---
+++ app/src/main/java/com/sangmoki/lifequotes/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.sangmoki.lifequotes
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.sangmoki.lifequotes.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
