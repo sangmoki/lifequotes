60f694b8e932778b2e671f963ce1ef45c6c1f45d
이상목 <yoochang4176@naver.com>
2024-05-23 15:09:56 +0900

    Refator: "리스트 뷰 어댑터 설정"

--- app/src/main/java/com/sangmoki/lifequotes/ListViewAdapter.kt
+++ app/src/main/java/com/sangmoki/lifequotes/ListViewAdapter.kt
@@ -1,4 +1,35 @@
 package com.sangmoki.lifequotes
 
-class ListViewAdapter {
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+class ListViewAdapter(val List: MutableList<String>) : BaseAdapter() {
+    override fun getCount(): Int {
+        return List.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return List[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+        var convertView = convertView
+
+        if (convertView == null) {
+            convertView = View.inflate(parent!!.context, R.layout.list_view_item, null)
+        }
+
+        val listviewText = convertView?.findViewById<TextView>(R.id.listViewItemTextView)
+        listviewText!!.text = List[position]
+
+        return convertView!!
+    }
+
 }
